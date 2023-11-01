a78e3371db2b14e0941635442bb3d9383b425109
panakir <panakir.webdev@gmail.com>
2023-11-01 23:35:58 +0300

    refactor: change header and footer to functional components (Wen, Nov 1, 2023 23:35)

--- react-app/src/components/header/Header.tsx
+++ react-app/src/components/header/Header.tsx
@@ -1,17 +1,14 @@
 import './header.scss';
-import { Component } from 'react';
 
-export class Header extends Component {
-  render() {
-    return (
-      <header className="header">
-        <img className="logo header__logo" src="./logo.png" alt="logo" />
-        <div className="header__wrapper">
-          <h1 className="header__title">
-            here you can find out information about ships from Star Wars
-          </h1>
-        </div>
-      </header>
-    );
-  }
+export const Header = () => {
+  return (
+    <header className="header">
+      <img className="logo header__logo" src="./logo.png" alt="logo" />
+      <div className="header__wrapper">
+        <h1 className="header__title">
+          here you can find out information about ships from Star Wars
+        </h1>
+      </div>
+    </header>
+  );
 }
