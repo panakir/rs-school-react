52b24a4fa8f85108aa65d3770d5ad197b1c88600
panakir <panakir.webdev@gmail.com>
2023-11-06 01:13:28 +0300

    feat: implement pagination (Mon, Nov 06, 2023 01:13AM)

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,10 +1,18 @@
 import './main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import { ErrorPage } from './components/errorPage/ErrorPage';
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  {},
+  { path: '*', element: <ErrorPage /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
