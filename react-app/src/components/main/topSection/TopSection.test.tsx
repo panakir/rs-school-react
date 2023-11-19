65d6c4c2a382f695ac6746ebe1726ebd6a2699d7
panakir <panakir.webdev@gmail.com>
2023-11-19 23:03:52 +0300

    fix: fix TopSection tests (Sun, Nov 19, 2023 23:03)

--- react-app/src/components/main/topSection/TopSection.test.tsx
+++ react-app/src/components/main/topSection/TopSection.test.tsx
@@ -1,38 +1,23 @@
-import { describe, it, expect, vi, Mock } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { TopSection } from './TopSection';
-import { MainSectionContext } from '../../context/context';
-
-const renderTopSectionWithContext = (): Mock => {
-  const mockedHandleSearch = vi.fn();
-
-  render(
-    <MainSectionContext.Provider
-      value={{
-        resultsOnPage: 10,
-        currentPage: 1,
-        setCurrentPage: (): void => {},
-        countAllResults: 0,
-        setCountAllResults: (): void => {},
-        resultOfSearch: [],
-        setResultOfSearch: () => [],
-        handleSearch: mockedHandleSearch,
-      }}
-    >
-      <TopSection />
-    </MainSectionContext.Provider>
-  );
-
-  return mockedHandleSearch;
-};
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from '../../../store';
 
 describe('TopSection', (): void => {
   const INPUT_PLACEHOLDER = 'Enter the name of ship';
   const USER_QUESTION = 'corvette';
 
   it('user writes text in search input', async () => {
-    renderTopSectionWithContext();
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <TopSection />
+        </Provider>
+      </BrowserRouter>
+    );
 
     const input: HTMLInputElement =
       screen.getByPlaceholderText(INPUT_PLACEHOLDER);
@@ -42,24 +27,18 @@ describe('TopSection', (): void => {
     expect(input.value).toBe(USER_QUESTION);
   });
 
-  it('user clicks on search button', async (): Promise<void> => {
-    const mockedHandleSearch = renderTopSectionWithContext();
-
-    await userEvent.click(screen.getByText('search'));
-
-    expect(mockedHandleSearch.mock.calls[0][0]).toBe('');
-  });
-
-  it(`user asks "${USER_QUESTION}"`, async (): Promise<void> => {
-    const mockedHandleSearch = renderTopSectionWithContext();
-
-    await userEvent.type(
-      screen.getByPlaceholderText(INPUT_PLACEHOLDER),
-      USER_QUESTION
+  it('Verify that clicking the Search button saves the entered value to the local storage', () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <TopSection />
+        </Provider>
+      </BrowserRouter>
     );
+    const input = screen.getByRole('textbox');
+    fireEvent.change(input, { target: { value: USER_QUESTION } });
 
-    await userEvent.click(screen.getByText('search'));
-
-    expect(mockedHandleSearch.mock.calls[0][0]).toBe(USER_QUESTION);
+    const localStorageValue = localStorage.getItem('searchRequest');
+    expect(localStorageValue).toBe(USER_QUESTION);
   });
 });
--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -8,10 +8,10 @@ import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
+    <BrowserRouter>
+      <Provider store={store}>
         <App />
-      </BrowserRouter>
-    </Provider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
